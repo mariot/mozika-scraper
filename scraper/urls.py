a352894123b683d10f1adb99887dea458ec3ea6b
Mariot Tsitoara <mariot@adbreakfast.com>
2017-10-11 23:31:28 +0300

    artists scrap

--- scraper/urls.py
+++ scraper/urls.py
@@ -1,4 +1,5 @@
 from rest_framework.routers import SimpleRouter
+from django.conf.urls import patterns, url
 from scraper import views
 
 
@@ -7,4 +8,4 @@ router = SimpleRouter()
 router.register(r'artist', views.ArtistViewSet, 'Artist')
 router.register(r'song', views.SongViewSet, 'Song')
 
-urlpatterns = router.urls
+urlpatterns = router.urls + patterns('', url(r'^scrap/(?P<page>\d+)/$', 'scraper.views.scrap'))
--- scraper/views.py
+++ scraper/views.py
@@ -3,6 +3,43 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from scraper.serializers import ArtistSerializer, SongSerializer
 from scraper.models import Artist, Song
+from django.http import HttpResponse
+import requests
+import bs4
+import json
+
+
+def get_artists(page):
+    artists_page = requests.get('http://tononkira.serasera.org/tononkira/mpihira/results/' + str(page))
+    artists_page.raise_for_status()
+
+    artists_object = bs4.BeautifulSoup(artists_page.text, "html.parser")
+
+    artists = []
+
+    artists_ugly = artists_object.select('td a')
+    artists_ugly_length = len(artists_ugly) / 3
+
+    for i in range(artists_ugly_length):
+        artist = {}
+        artist['name'] = artists_ugly[i * 3].getText()
+        artist['songsURL'] = artists_ugly[(i * 3) + 1].get('href')
+        artists.append(artist)
+
+    return artists
+
+def scrap(request, page):
+    page_in_url = page * 20
+
+    artists = get_artists(page_in_url)
+
+    for artist in artists:
+        post_data = {'name': artist['name']}
+        response = requests.post('https://mozikascraper.herokuapp.com/scraper/artist/', data=post_data)
+        artist_id = json.loads(response.content)['id']
+
+    html = "<html><body>Page: %s.</body></html>" % page
+    return HttpResponse(html)
 
 
 class ArtistViewSet(ViewSet):
