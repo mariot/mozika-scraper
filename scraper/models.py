e92b129d5cb8e3441d5910e1a61908ec2d8773f7
Mariot Tsitoara <mariot@hianatra.com>
2018-12-24 21:46:03 +0000

    increase text max_length

--- scraper/models.py
+++ scraper/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 
 class Artist(models.Model):
-    name = models.CharField(max_length=100)
-    url = models.CharField(max_length=100, default='')
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=200, default='')
     songs_id = models.IntegerField(blank=True, null=True)
     number_of_songs = models.IntegerField(blank=True, default=0)
     hits = models.IntegerField(blank=True, default=0)
@@ -16,8 +16,8 @@ class Artist(models.Model):
 
 class Song(models.Model):
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    url = models.CharField(max_length=100, default='')
+    title = models.CharField(max_length=200)
+    url = models.CharField(max_length=200, default='')
     lyrics = models.TextField()
     hits = models.IntegerField(blank=True, default=0)
 
@@ -26,16 +26,16 @@ class Song(models.Model):
 
 
 class User(models.Model):
-    fbid = models.CharField(max_length=100)
-    name = models.CharField(max_length=100, blank=True)
+    fbid = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return self.fbid
 
 
 class MissingSong(models.Model):
-    artist = models.CharField(max_length=100)
-    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
 
     def __str__(self):
         return self.title + " - " + self.artist
